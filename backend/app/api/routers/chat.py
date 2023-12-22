88727c2476aa2b71d7d039b6e998dde5e1c2454b
AndrewKPascual <95746939+AndrewKPascual@users.noreply.github.com>
2023-12-22 06:53:15 -0800

    fix

--- backend/app/api/routers/chat.py
+++ backend/app/api/routers/chat.py
@@ -1,68 +1,48 @@
-from typing import List
-
-from fastapi.responses import StreamingResponse
-
-from app.utils.json import json_to_model
-from app.utils.index import get_index
-from fastapi import APIRouter, Depends, HTTPException, Request, status
-from llama_index import VectorStoreIndex
-from llama_index.llms.base import  ChatMessage
-from llama_index.llms.types import MessageRole
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from typing import List
+from llama_index.llms.types import MessageRole
+from llama_index.llms.base import ChatMessage
+# Make sure to import VectorStoreIndex correctly based on your project's structure.
 
-
-
-chat_router = r = APIRouter()
-
+app = FastAPI()  # Use FastAPI's instance
 
 class _Message(BaseModel):
     role: MessageRole
     content: str
 
-
 class _ChatData(BaseModel):
     messages: List[_Message]
 
+# Define a function to handle the chat logic.
+async def handle_chat(chat_engine, last_message, messages):
+    # Implement this function to handle the chat logic.
+    # For example, you might call chat_engine.stream_chat(...) here,
+    # but remember to adapt it to return the full response rather than streaming.
+    pass
 
-@r.post("")
-async def chat(
-    request: Request,
-    # Note: To support clients sending a JSON object using content-type "text/plain",
-    # we need to use Depends(json_to_model(_ChatData)) here
-    data: _ChatData = Depends(json_to_model(_ChatData)),
-    index: VectorStoreIndex = Depends(get_index),
-):
-    # check preconditions and get last message
+@app.post("/")  # Route at the root of the serverless function
+async def chat(data: _ChatData):
     if len(data.messages) == 0:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="No messages provided",
         )
-    lastMessage = data.messages.pop()
-    if lastMessage.role != MessageRole.USER:
+    last_message = data.messages.pop()
+    if last_message.role != MessageRole.USER:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Last message must be from user",
         )
-    # convert messages coming from the request to type ChatMessage
-    messages = [
-        ChatMessage(
-            role=m.role,
-            content=m.content,
-        )
-        for m in data.messages
-    ]
 
-    # query chat engine
+    messages = [ChatMessage(role=m.role, content=m.content) for m in data.messages]
+
+    # Initialize your chat engine with the correct parameters.
+    index = VectorStoreIndex(...)  # Replace ... with actual initialization parameters.
     chat_engine = index.as_chat_engine(chat_mode='condense_plus_context', verbose=True)
-    response = chat_engine.stream_chat(lastMessage.content, messages)
 
-    # stream response
-    async def event_generator():
-        for token in response.response_gen:
-            # If client closes connection, stop sending events
-            if await request.is_disconnected():
-                break
-            yield token
+    # Call the handle_chat function to process the messages and generate a response.
+    response = await handle_chat(chat_engine, last_message, messages)
 
-    return StreamingResponse(event_generator(), media_type="text/plain")
+    # Return the response in a format that the frontend expects.
+    return {"response": response}
